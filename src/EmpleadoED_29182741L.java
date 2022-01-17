40dcc34629af0c90d710041889fdb0701a5e0492
BeaZahonero <beazaho@hotmail.com>
2022-01-17 16:09:15 +0100

    versión final con javadoc DNI 29182741L

--- src/EmpleadoED_29182741L.java
+++ src/EmpleadoED_29182741L.java
@@ -1,24 +1,37 @@
 
 /**
- * Descripción de la clase Empleado:
+ * Descripción: EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED
  *
- * @version 1.0
- * @author Bea Zahonero
+ * @author Bea Zahonero DNI 29182741L
  * @author <a href="mailto:beazaho@hotmail.com">beazaho@hotmail.com</a>
- * 17 ene 2022 15:47:50
+ * @version 1.0 17 ene 2022 15:47:50
  */
-// !!!! MODIFICA TU NOMBRE Y TU DNI DONDE TOQUE
 public class EmpleadoED_29182741L {
 
    static final double FACTOR_HORA_EXTRA = 1.2;
    private String nombreCompleto, dni;
    private double salarioBasePorHora = 10.56;
 
+   /**
+    * Constructor con DOS parámetros
+    *
+    * @param nombreCompleto nombreCompleto nombre completo en mayúsculas
+    * @param dni dni documento nacional de identidad con letra y sin espacios
+    */
    public EmpleadoED_29182741L(String nombreCompleto, String dni) {
       this.nombreCompleto = nombreCompleto;
       this.dni = dni;
    }
 
+   /**
+    * Calcula el salario bruto mensual en función del salario base por hora y
+    * las horas extras. El precio por hora de la hora extra se determina con la
+    * constante FACTOR_HORA_EXTRA
+    *
+    * @param horasExtra horasExtra número de horas extra dedicadas redondeado a
+    * la baja (entero)
+    * @return devuelve el salario bruto mensual en euros con dos decimales
+    */
    public double getSalarioEsteMes(int horasExtra) {
       double cantidadExtra, salarioFinal;
       cantidadExtra = horasExtra * this.salarioBasePorHora
